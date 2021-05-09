f264614013e43ad9c0cdea354c368f75a3434a84
koller-m <mkoller@eml.cc>
2021-05-08 18:00:02 -0600

    Add header to tip percentage section Create computed property for calculations

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -14,6 +14,19 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -26,15 +39,16 @@ struct ContentView: View {
                         }
                     }
                 }
-                Section {
+                Section(header: Text("How much tip do you want to leave?")) {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0..<tipPercentages.count) {
                             Text("\(self.tipPercentages[$0])%")
                         }
                     }
+                    .pickerStyle(SegmentedPickerStyle())
                 }
                 Section {
-                    Text("$\(checkAmount)")
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
             }
             .navigationBarTitle("WeSplit")
